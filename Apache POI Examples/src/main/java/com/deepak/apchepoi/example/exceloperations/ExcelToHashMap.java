217b5d722f16df72c4774a078da1488afb5ea978
Deepk kumar <deepak.kumar@hp.com>
2021-06-19 13:39:48 +0530

    added Color to style cell

---
+++ Apache POI Examples/src/main/java/com/deepak/apchepoi/example/exceloperations/ExcelToHashMap.java
@@ -0,0 +1,54 @@
+package com.deepak.apchepoi.example.exceloperations;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+/**
+ * Created by deepak on 19-06-2021.
+ */
+
+public class ExcelToHashMap {
+
+	public static void main(String[] args) throws IOException {
+		
+		FileInputStream fis=new FileInputStream(".\\datafiles\\student.xlsx");
+		XSSFWorkbook workbook=new XSSFWorkbook(fis);
+		XSSFSheet sheet=workbook.getSheet("Student data");
+		
+		int rows=sheet.getLastRowNum();
+		
+		Map<String,String> data=new HashMap<String,String>();
+		
+		//Reading data from excel to HashMap
+		for(int r=0;r<=rows;r++)
+		{
+			String key=sheet.getRow(r).getCell(0).getStringCellValue();
+			String value=sheet.getRow(r).getCell(1).getStringCellValue();
+			data.put(key, value);
+			
+		}
+		
+		//Read data from HashMap
+		
+		for(Map.Entry entry:data.entrySet())
+		{
+			System.out.println(entry.getKey()+"   "+entry.getValue());
+		}
+		
+	}
+
+}
+
+
+
+
+
+
+
+
